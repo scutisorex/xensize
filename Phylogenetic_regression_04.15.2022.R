9170f59731bfbebbd22f98891e730d0ae4e85b48
Stephanie Smith <smsmith@fieldmuseum.org>
2022-06-13 10:58:37 -0500

    Steph's edits to PGLS code and plots

--- Phylogenetic_regression_04.15.2022.R
+++ Phylogenetic_regression_04.15.2022.R
@@ -5,24 +5,9 @@ library(ape)
 library(phytools)
 library(nlme)
 library(geiger)
+library(caper)
 
 
-# mammal.tree<-read.tree("mammalHR.phy.txt")
-# plotTree(mammal.tree,fsize=0.8,ftype="i", lwd=1)
-# 
-# mammal.data<-read.csv("mammalHR.csv", row.names=1)
-# mammal.data
-# 
-# 
-# bodyMass<-setNames(log(mammal.data$bodyMass),rownames(mammal.data))
-# phylosig(mammal.tree,bodyMass)
-# bmassK<-phylosig(mammal.tree, bodyMass,test=TRUE,nsim=10000)
-# bmassK
-# 
-# phylosig(mammal.tree,bodyMass, method="lambda")
-
-
-#xdata[,1] = as.character(xdata[,1]) 
 
 candy <- c('#46291b', '#801216', '#cc9a69', '#ad9334', '#c4255b', '#ee97a8', '#94958f', '#2d8f8e', '#ddbdae', '#316f5a')
 ##          bra       chla        cho         cyc       das         myr       pri         tam         tol
@@ -31,7 +16,7 @@ data <- read.csv("PGLS_Regression_Data.csv", header = T)
 #data_log <- data %>% add_column(bv.tv.log = log(data$BV.TV)) %>% add_column(proxy.log = log(data$mass_proxy)) %>% add_column(tb.n.log = log(data$TB.N)) %>% add_column(tb.th.log = log(data$TB.TH)) %>% add_column(conn.log = log(data$Connectivity)) %>% add_column(conn.d.log = log(data$Conn.D)) %>% add_column(csa.log = log(data$CSA)) %>% add_column(gc.log = log(data$GC)) %>% add_column(mil.log = log(data$MIL)) %>% add_column(sld.log = log(data$SLD))%>% add_column(mass.log = log(data$mass)) %>% add_column(cw.log = log(data$CW))%>% add_column(izl.log = log(data$IZL))
 #data_log <- data_log %>% arrange(desc(row_number()))
 
-######## Hey: this command does the same thing as the above chunk of code, with a lot fewer opportunities for error, and therefore less debugging!
+######## Note: this command does the same thing as the above chunk of code, with a lot fewer opportunities for error, and therefore less debugging! It does change some of the column names but I fixed them in the versions for ps1 below.
 data_log <- data %>% 
   mutate_at(c(5:17), log) %>% 
   arrange(desc(row_number()))
@@ -44,40 +29,48 @@ ps1 <- data_log %>%
 rownames(ps1) <- ps1$Taxon
 
 
+
 #ps2
-ps2 <- data_log %>% filter(pos.for.analysis == "ps2")
-ps2 <- ps2 %>% arrange(desc(row_number()))
+ps2 <- data_log %>% 
+  filter(pos.for.analysis == "ps2")%>% 
+  arrange(desc(row_number()))
 rownames(ps2) <- ps2$Taxon
 
 
 #ps3
-ps3 <- data_log %>% filter(pos.for.analysis == "ps3")
-ps3 <- ps3 %>% arrange(desc(row_number()))
+ps3 <- data_log %>% 
+  filter(pos.for.analysis == "ps3")%>% 
+  arrange(desc(row_number()))
 rownames(ps3) <- ps3$Taxon
 
 
 #ps4
-ps4 <- data_log %>% filter(pos.for.analysis == "ps4")
-ps4 <- ps4 %>% arrange(desc(row_number()))
+ps4 <- data_log %>% 
+  filter(pos.for.analysis == "ps4")%>% 
+  arrange(desc(row_number()))
 rownames(ps4) <- ps4$Taxon
 
 
 #ps5
-ps5 <- data_log %>% filter(pos.for.analysis == "ps5")
-ps5 <- ps5 %>% arrange(desc(row_number()))
+ps5 <- data_log %>% 
+  filter(pos.for.analysis == "ps5") %>% 
+  arrange(desc(row_number()))
 rownames(ps5) <- ps5$Taxon
 
 
 #ps6
-ps6 <- data_log %>% filter(pos.for.analysis == "ps6")
-ps6 <- ps6 %>% arrange(desc(row_number()))
+ps6 <- data_log %>% 
+  filter(pos.for.analysis == "ps6") %>% 
+  arrange(desc(row_number()))
 rownames(ps6) <- ps6$Taxon
 
 
 
 
 #Creating the phylogeny
-guys <- read.csv(file = "/Users/elliezack/Desktop/Size Paper/Data and Graphs/specieswithecology.csv", header = T)
+guys <- read.csv(file = "specieswithecology.csv", header = T) %>% 
+  rename(taxon = ï..taxon) # I had to rename the column with the taxa in it for some reason, my computer didn't read it right!
+
 rownames(guys) <- guys$taxon
 bot <- read.tree(file = "Gibbsetal2015_tree.newick.txt")
 plot(bot)
@@ -88,17 +81,33 @@ comp <- name.check(bot, guys)
 ins <- drop.tip(bot, comp$tree_not_data)
 write.tree(ins, file = 'OG_Tree_species.txt')
 plot(ins)
-
 name.check(ins, ps1)
 
 
 # generalized linear model
-bm<-corBrownian(1, ins)
 
-bvtv1<-gls(CL~BV.TV, data=ps1, correlation=bm)
+# Steph's edits for ps1:
+
+# Run the PGLS: 
+#-------------------NOTE!!! You couldn't see the lines because your variables were in the wrong order!-------------------------#
+
+bvtv1<-gls(BV.TV~IZL, data=ps1, correlation=corBrownian(1, phy = ins, form = ~Taxon))# You need the "form" term because of some update to the package in the last couple years. It gives you a warning if you don't have it. And it refers back to the data, so I think you have to include it in the gls command itself in full so that it can refer to it. It works fine inside your loop (see below).
 summary(bvtv1)
-plot(bvtv1)
-plot(bvtv1)
+
+# Run a linear model without the phy correlation:
+nophybvtv <- gls(BV.TV~IZL, data=ps1)
+
+# plot the log-transformed variables:
+plot(BV.TV~IZL, data = data_log, pch = 16)
+
+# plot the regular GLS, teal
+abline(nophybvtv,lwd = 7, col = "#edb742")
+
+# plot the PGLS, tangerine!
+abline(bvtv1, lwd = 7, col = "#0e8c90")
+
+
+
 bvtv2<-gls(CL~BV.TV, data=ps2, correlation=bm)
 summary(bvtv2)
 bvtv3<-gls(CL~BV.TV, data=ps3, correlation=bm)
@@ -125,7 +134,7 @@ summary(bvtv6)
 pos <- c('ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6')
 for (i in 1:6){
   ps <- data_log %>% filter(pos.for.analysis == pos[i])
-  gls <- gls(proxy.log~bv.tv.log, data = ps, correlation = bm)
+  gls <- gls(BV.TV~mass_proxy, data = ps, correlation = corBrownian(1, phy = ins, form = ~Taxon)) #note I added the "form" term here and switched the order of variables
   print(summary(gls))
 }
 
